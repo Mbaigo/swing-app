814d41024308e7c8122a737199df53ac7e3ee740
Mbaigo <mbaigolmemevrard@gmail.com>
2026-01-18 21:48:09 +0400

    firt commit wiht Client future

---
+++ src/test/java/com/mbaigo/datecentre/swingApp/SwingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbaigo.datecentre.swingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
